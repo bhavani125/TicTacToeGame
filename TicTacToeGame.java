c1d03236020a7a3f1db477d7a63f9d5f7612da9b
bhavani125 <girinenibhavani12@gmil.com>
2021-07-08 15:39:23 +0530

    Added: code for player to choose the option X or O

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,13 +1,21 @@
 package com.game;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
+    //constant variables
     static char[] board = new char[10];
-     //Main Method
+    static char player, computer;
+    static Scanner sc = new Scanner(System.in);
+
+    //Main Method
     public static void main(String[] args) {
         System.out.println("Welcome to TicTacToeGame program ");
 
         // calling creatingBoard method
         creatingBoard();
+        //calling playerChoice method
+        playerChoice();
 
     }
 
@@ -15,7 +23,20 @@ public class TicTacToeGame {
     static void creatingBoard() {
         for (int i = 1; i < 10; i++) {
 
-            board[i] = ' ';
+            board[i] = ' '; //initializing empty space
         }
     }
+
+    //creating method for choosing option
+    static void playerChoice() {
+        System.out.println("choose the letter X or O ");
+        player = sc.next().charAt(0);
+        if (player == 'X') {
+            computer = 'O';
+        } else {
+            computer = 'X';
+        }
+
+    }
 }
+
